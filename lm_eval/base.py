9a8fb9226c960d8058dd5b1ce517c77fa2a5e3f4
gk <gkpyon@gmail.com>
2023-06-09 09:21:57 +0900

    wip2

--- lm_eval/base.py
+++ lm_eval/base.py
@@ -721,6 +721,9 @@ class MultipleChoiceTask(Task):
     def process_results(self, doc, results):
         gold = doc["gold"]
 
+        if len(torch.unique(results)) != len(results):
+            print(results)
+
         acc = 1.0 if np.argmax(results) == gold else 0.0
         completion_len = np.array([float(len(i)) for i in doc["choices"]])
         acc_norm = 1.0 if np.argmax(results / completion_len) == gold else 0.0
